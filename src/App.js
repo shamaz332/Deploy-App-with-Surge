b3d55de4a06bf12d19edbd857b03d60e8ffe729a
Shamaz saeed <40767080+shamaz332@users.noreply.github.com>
2020-06-01 21:20:52 +0500

    edit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+        Welcome to second class of bootcamp
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         
         </a>
       </header>
     </div>
